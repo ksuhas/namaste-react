b70085ef50f24329746ea4e3df280495fefd7582
Suhas Khulpe <suhaskhulpe146@gmail.com>
2023-04-27 20:11:23 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,24 +1,7 @@
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-
-
-// Nested Structure
-// const parent = React.createElement("div", {id:"parent"},
-//  React.createElement("div", {id:"child"},
-//  React.createElement("h1", {}, "I am h1 tag")
-//  )
-// );
-
-
-// Nested Structure with siblings
-// const parent = React.createElement("div", {id:"parent"},
-//  React.createElement("div", {id:"child"}, 
-//  [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")]
-//  ) 
-// );
-
-
-// More Nested and complex Structure with siblings
 const parent = React.createElement("div", { id: "parent" },
     [
         React.createElement("div", { id: "child1" },
@@ -33,9 +16,5 @@ const parent = React.createElement("div", { id: "parent" },
 
 console.log(parent);
 
-
-
-
-// const heading = React.createElement("h1", {id:"heading", xyz:"abc"}, "Hello World from React!");
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 Namaste React
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algorithm
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compression
+- Consistant Hashing
+- Code Splitting
+- Differential Bundling - Support older browsers
+- Diagnostics 
+- Error Handling
+- HTTP's support
+- Tree Shaking - Remove Unused Code
+- Different Dev and Prod bundles   
