3aefbe41b737cdae7fe0a50f46b467b361857be1
Suhas Khulpe <suhaskhulpe146@gmail.com>
2023-04-28 13:39:31 +0530

    episode-03

--- App.js
+++ App.js
@@ -2,19 +2,84 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement("div", { id: "parent" },
-    [
-        React.createElement("div", { id: "child1" },
-            [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")]
-        )
-        ,
-        React.createElement("div", { id: "child2" },
-            [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")]
-        )
-    ] 
+// React Element
+// const heading = React.createElement("h1", {id : "heading"}, "Namaste React");
+// console.log(heading);
+
+// // JSX Element
+// const JSXHeading = <h1 className="head">Namaste React from JSX</h1>;
+// console.log(JSXHeading);
+
+// React Funcational Component
+// const HeadingComponent = () => {
+//     return <h1>Namaste React from Functional Component</h1>;
+// };
+
+// above and below syntax is same
+
+// const HeadingComponent = () => (
+//         <h1>Namaste React from Functional Component</h1>
+// );
+
+
+// Component Composition
+
+// const Title = () => (
+//     <h1 className="head">Namaste React from JSX</h1>
+// );
+
+// 
+// const HeadingComponent = () => (
+//     <div id="container">
+//          <Title/>
+//         <h2>{200 + 300}</h2> 
+//         <h1 className='head'>Namaste React from Functional Component</h1>
+//     </div>
+// );
+
+
+
+// Adding React Element in Component
+// const title = (
+//     <h1 className="head">Namaste React from JSX</h1>
+// );
+
+// const HeadingComponent = () => (
+//     <div id="container">
+//         {title}
+//         <h1 className='head'>Namaste React from Functional Component</h1>
+//     </div>
+// );
+
+// Calling the component with function call
+// const Title = () => (
+//     <h1 className="head">Namaste React from JSX</h1>
+// );
+
+
+// const HeadingComponent = () => (
+//     <div id="container">
+//         {Title()}
+//         <h1 className='head'>Namaste React from Functional Component</h1>
+//     </div>
+// );
+
+// React Fragments
+const Title = () => (
+    <h1 className="head">Namaste React from JSX</h1>
 );
 
-console.log(parent);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+const HeadingComponent = () => (
+    <>
+        <div id="container">
+            {Title()}
+            <h1 className='head'>Namaste React from Functional Component</h1>
+        </div>
+        <div id="container">
+        </div>
+    </>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>);
